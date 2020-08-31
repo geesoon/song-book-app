41e86c350a2f077947f35c571288ec33016ffe16
geesoon <hiewgs@gmail.com>
2020-08-31 09:26:54 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div id="navbar" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bg-light align-items-center">
+        <h1
+          class="navbar-brand text-primary"
+          style="cursor: pointer"
+          @click="resetPage"
+        >Song Book App</h1>
+      </div>
+    </header>
+    <Song :reset="reset" @pageReset="updateReset"></Song>
+    <vue-fab mainBtnColor="#3599DB" v-scroll-to="'#app'" icon="arrow_upward"></vue-fab>
+    <footer class="footer">
+      <span class="text-muted">@2020 Song Book App</span>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Song from "./components/song";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Song,
+  },
+  data() {
+    return { reset: false };
+  },
+  methods: {
+    resetPage() {
+      this.reset = true;
+      console.log("Title Clicked!", this.reset);
+    },
+    updateReset(event) {
+      this.reset = event;
+      console.log("App.vue", event);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  height: min-vh-100;
+}
+
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: var(--main-font-family);
+}
+
+div #navbar {
+  padding: 10px;
+  font-family: var(--main-font-family);
+}
+
+.footer {
+  font-family: var(--main-font-family);
+  width: 100%;
+  position: relative;
+  line-height: 50px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+  text-align: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vueScrollto from "vue-scrollto";
+import VueFab from "vue-float-action-button";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(vueScrollto);
+Vue.use(VueFab);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
